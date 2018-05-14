20ee4a40cd1d5ea92aa6c31da36ec08201aa6620
dougzilla <dougzilla32@gmail.com>
2018-05-13 23:11:42 -0700

    Fix problem of circular references with CancelContext
    Simplify CancelContext interface
    
    Add 'firstly' 'missing cancel context' error message
    
    Simplify 'CPKAlamofire' to just wrap the promise returned from 'PMKAlamofire'
    
    Update 'CPKFoundation' for simplified CancelContext interface
    
    Check-in the CPKFoundation build scheme

--- Sources/Alamofire+CancellablePromise.swift
+++ Sources/Alamofire+CancellablePromise.swift
@@ -13,155 +13,63 @@ extension Request: CancellableTask {
 }
 
 extension Alamofire.DataRequest {
-    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(URLRequest, HTTPURLResponse, Data)> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.response(queue: queue) { rsp in
-                if let error = rsp.error {
-                    seal.reject(error)
-                } else if let a = rsp.request, let b = rsp.response, let c = rsp.data {
-                    seal.fulfill((a, b, c))
-                } else {
-                    seal.reject(PMKError.invalidCallingConvention)
-                }
+    func wrap<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
+        return Promise(cancel: cancel ?? CancelContext(), task: self) {seal in
+            promise.done {
+                seal.fulfill($0)
+                }.catch {
+                    seal.reject($0)
             }
         }
+    }
+
+    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(URLRequest, HTTPURLResponse, Data)> {
+        return wrap(self.response(.promise, queue: queue), cancel: cancel)
     }
     
     public func responseDataCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(data: Data, response: PMKAlamofireDataResponse)> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.responseData(queue: queue) { response in
-                switch response.result {
-                case .success(let value):
-                    seal.fulfill((value, PMKAlamofireDataResponse(response)))
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return wrap(self.responseData(queue: queue), cancel: cancel)
     }
     
     public func responseStringCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(string: String, response: PMKAlamofireDataResponse)> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.responseString(queue: queue) { response in
-                switch response.result {
-                case .success(let value):
-                    seal.fulfill((value, PMKAlamofireDataResponse(response)))
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return wrap(self.responseString(queue: queue), cancel: cancel)
     }
     
     public func responseJSONCC(queue: DispatchQueue? = nil, options: JSONSerialization.ReadingOptions = .allowFragments, cancel: CancelContext? = nil) -> Promise<(json: Any, response: PMKAlamofireDataResponse)> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.responseJSON(queue: queue, options: options) { response in
-                switch response.result {
-                case .success(let value):
-                    seal.fulfill((value, PMKAlamofireDataResponse(response)))
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return wrap(self.responseJSON(queue: queue, options: options), cancel: cancel)
     }
     
     public func responsePropertyListCC(queue: DispatchQueue? = nil, options: PropertyListSerialization.ReadOptions = PropertyListSerialization.ReadOptions(), cancel: CancelContext? = nil) -> Promise<(plist: Any, response: PMKAlamofireDataResponse)> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.responsePropertyList(queue: queue, options: options) { response in
-                switch response.result {
-                case .success(let value):
-                    seal.fulfill((value, PMKAlamofireDataResponse(response)))
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return wrap(self.responsePropertyList(queue: queue, options: options), cancel: cancel)
     }
     
     #if swift(>=3.2)
-    /**
-     Returns a Promise for a Decodable
-     Adds a handler to be called once the request has finished.
-     
-     - Parameter queue: DispatchQueue, by default nil
-     - Parameter decoder: JSONDecoder, by default JSONDecoder()
-     */
     public func responseDecodableCC<T: Decodable>(queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder(), cancel: CancelContext? = nil) -> Promise<T> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.responseData(queue: queue) { response in
-                switch response.result {
-                case .success(let value):
-                    do {
-                        seal.fulfill(try decoder.decode(T.self, from: value))
-                    } catch {
-                        seal.reject(error)
-                    }
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return wrap(self.responseDecodable(queue: queue, decoder: decoder), cancel: cancel)
     }
     
-    /**
-     Returns a Promise for a Decodable
-     Adds a handler to be called once the request has finished.
-     
-     - Parameter queue: DispatchQueue, by default nil
-     - Parameter decoder: JSONDecoder, by default JSONDecoder()
-     */
     public func responseDecodableCC<T: Decodable>(_ type: T.Type, queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder(), cancel: CancelContext? = nil) -> Promise<T> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.responseData(queue: queue) { response in
-                switch response.result {
-                case .success(let value):
-                    do {
-                        seal.fulfill(try decoder.decode(type, from: value))
-                    } catch {
-                        seal.reject(error)
-                    }
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return wrap(self.responseDecodable(type, queue: queue, decoder: decoder), cancel: cancel)
     }
     #endif
 }
 
 extension Alamofire.DownloadRequest {
-    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<DefaultDownloadResponse> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.response(queue: queue) { response in
-                if let error = response.error {
-                    seal.reject(error)
-                } else {
-                    seal.fulfill(response)
-                }
+    func wrap<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
+        return Promise(cancel: cancel ?? CancelContext(), task: self) {seal in
+            promise.done {
+                seal.fulfill($0)
+            }.catch {
+                seal.reject($0)
             }
         }
     }
+
+    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<DefaultDownloadResponse> {
+        return wrap(self.response(.promise, queue: queue), cancel: cancel)
+    }
     
     public func responseDataCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<DownloadResponse<Data>> {
-        let cancelContext = cancel ?? CancelContext()
-        return Promise(cancel: cancelContext, task: self) { seal in
-            self.responseData(queue: queue) { response in
-                switch response.result {
-                case .success:
-                    seal.fulfill(response)
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
+        return wrap(self.responseData(queue: queue), cancel: cancel)
     }
 }
