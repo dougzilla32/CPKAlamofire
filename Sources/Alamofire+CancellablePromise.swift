206edc1a9c6aa73fbaddac3329a8429af12a9d3b
dougzilla <dougzilla32@gmail.com>
2018-05-24 23:57:33 -0700

    Convert Alamofire extension to CancellablePromise API

--- Sources/Alamofire+CancellablePromise.swift
+++ Sources/Alamofire+CancellablePromise.swift
@@ -21,54 +21,40 @@ extension Alamofire.DataRequest {
         }
     }
     
-   func wrapCC<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
-        return Promise(cancel: cancel ?? CancelContext(), task: self) { seal in
-            promise.done {
-                seal.fulfill($0)
-            }.catch {
-                seal.reject($0)
-            }
-        }
-    }
-
     public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil) -> CancellablePromise<(URLRequest, HTTPURLResponse, Data)> {
         return wrap(self.response(.promise, queue: queue))
     }
     
-    public func responseCN(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(URLRequest, HTTPURLResponse, Data)> {
-        return wrapCC(self.response(.promise, queue: queue), cancel: cancel)
+    public func responseDataCC(queue: DispatchQueue? = nil) -> CancellablePromise<(data: Data, response: PMKAlamofireDataResponse)> {
+        return wrap(self.responseData(queue: queue))
     }
     
-    public func responseDataCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(data: Data, response: PMKAlamofireDataResponse)> {
-        return wrapCC(self.responseData(queue: queue), cancel: cancel)
+    public func responseStringCC(queue: DispatchQueue? = nil) -> CancellablePromise<(string: String, response: PMKAlamofireDataResponse)> {
+        return wrap(self.responseString(queue: queue))
     }
     
-    public func responseStringCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(string: String, response: PMKAlamofireDataResponse)> {
-        return wrapCC(self.responseString(queue: queue), cancel: cancel)
+    public func responseJSONCC(queue: DispatchQueue? = nil, options: JSONSerialization.ReadingOptions = .allowFragments) -> CancellablePromise<(json: Any, response: PMKAlamofireDataResponse)> {
+        return wrap(self.responseJSON(queue: queue, options: options))
     }
     
-    public func responseJSONCC(queue: DispatchQueue? = nil, options: JSONSerialization.ReadingOptions = .allowFragments, cancel: CancelContext? = nil) -> Promise<(json: Any, response: PMKAlamofireDataResponse)> {
-        return wrapCC(self.responseJSON(queue: queue, options: options), cancel: cancel)
-    }
-    
-    public func responsePropertyListCC(queue: DispatchQueue? = nil, options: PropertyListSerialization.ReadOptions = PropertyListSerialization.ReadOptions(), cancel: CancelContext? = nil) -> Promise<(plist: Any, response: PMKAlamofireDataResponse)> {
-        return wrapCC(self.responsePropertyList(queue: queue, options: options), cancel: cancel)
+    public func responsePropertyListCC(queue: DispatchQueue? = nil, options: PropertyListSerialization.ReadOptions = PropertyListSerialization.ReadOptions()) -> CancellablePromise<(plist: Any, response: PMKAlamofireDataResponse)> {
+        return wrap(self.responsePropertyList(queue: queue, options: options))
     }
     
     #if swift(>=3.2)
-    public func responseDecodableCC<T: Decodable>(queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder(), cancel: CancelContext? = nil) -> Promise<T> {
-        return wrapCC(self.responseDecodable(queue: queue, decoder: decoder), cancel: cancel)
+    public func responseDecodableCC<T: Decodable>(queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder()) -> CancellablePromise<T> {
+        return wrap(self.responseDecodable(queue: queue, decoder: decoder))
     }
     
-    public func responseDecodableCC<T: Decodable>(_ type: T.Type, queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder(), cancel: CancelContext? = nil) -> Promise<T> {
-        return wrapCC(self.responseDecodable(type, queue: queue, decoder: decoder), cancel: cancel)
+    public func responseDecodableCC<T: Decodable>(_ type: T.Type, queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder()) -> CancellablePromise<T> {
+        return wrap(self.responseDecodable(type, queue: queue, decoder: decoder))
     }
     #endif
 }
 
 extension Alamofire.DownloadRequest {
-    func wrapCC<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
-        return Promise(cancel: cancel ?? CancelContext(), task: self) {seal in
+    func wrap<T>(_ promise: Promise<T>) -> CancellablePromise<T> {
+        return CancellablePromise(task: self) { seal in
             promise.done {
                 seal.fulfill($0)
             }.catch {
@@ -77,11 +63,11 @@ extension Alamofire.DownloadRequest {
         }
     }
 
-    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<DefaultDownloadResponse> {
-        return wrapCC(self.response(.promise, queue: queue), cancel: cancel)
+    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil) -> CancellablePromise<DefaultDownloadResponse> {
+        return wrap(self.response(.promise, queue: queue))
     }
     
-    public func responseDataCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<DownloadResponse<Data>> {
-        return wrapCC(self.responseData(queue: queue), cancel: cancel)
+    public func responseDataCC(queue: DispatchQueue? = nil) -> CancellablePromise<DownloadResponse<Data>> {
+        return wrap(self.responseData(queue: queue))
     }
 }
--- Tests/TestAlamofire.swift
+++ Tests/TestAlamofire.swift
@@ -2,7 +2,7 @@ import Alamofire
 import CPKAlamofire
 import OHHTTPStubs
 import PromiseKit
-@testable import CancelForPromiseKit
+import CancelForPromiseKit
 import XCTest
 
 class AlamofireTests: XCTestCase {
@@ -15,15 +15,14 @@ class AlamofireTests: XCTestCase {
         
         let ex = expectation(description: "")
         
-        let context = CancelContext()
-        firstlyCC(cancel: context) {
+        firstly {
             Alamofire.request("http://example.com", method: .get).responseJSONCC()
-        }.doneCC { _ in
+        }.done { _ in
             XCTFail("failed to cancel request")
-        }.catchCC(policy: .allErrors) { error in
+        }.catch(policy: .allErrors) { error in
             error.isCancelled ? ex.fulfill() : XCTFail("Error: \(error)")
-        }
-        context.cancel()
+        }.cancel()
+
         waitForExpectations(timeout: 1)
     }
     
@@ -39,8 +38,8 @@ class AlamofireTests: XCTestCase {
     
     func testDecodable1() {
         
-        func getFixture(context: CancelContext) -> Promise<Fixture> {
-            return Alamofire.request("http://example.com", method: .get).responseDecodableCC(queue: nil, cancel: context)
+        func getFixture() -> CancellablePromise<Fixture> {
+            return Alamofire.request("http://example.com", method: .get).responseDecodableCC(queue: nil)
         }
         
         let json: NSDictionary = ["key1": "value1", "key2": ["value2A", "value2B"]]
@@ -51,16 +50,14 @@ class AlamofireTests: XCTestCase {
         
         let ex = expectation(description: "")
         
-        let context = CancelContext()
-        getFixture(context: context).doneCC { fixture in
+        getFixture().done { fixture in
             XCTAssert(fixture.key1 == "value1", "Value1 found")
             XCTFail("failed to cancel request")
-        }.catchCC(policy: .allErrors) { error in
+        }.catch(policy: .allErrors) { error in
             error.isCancelled ? ex.fulfill() : XCTFail("Error: \(error)")
-        }
-        context.cancel()
-        waitForExpectations(timeout: 1)
+        }.cancel()
         
+        waitForExpectations(timeout: 1)
     }
     
     func testDecodable2() {
@@ -72,16 +69,14 @@ class AlamofireTests: XCTestCase {
         
         let ex = expectation(description: "")
         
-        let context = CancelContext()
-        firstlyCC(cancel: context) {
+        firstly {
             Alamofire.request("http://example.com", method: .get).responseDecodableCC(Fixture.self)
-        }.doneCC { fixture in
+        }.done { fixture in
             XCTAssert(fixture.key1 == "value1", "Value1 found")
             XCTFail("failed to cancel request")
-        }.catchCC(policy: .allErrors) { error in
+        }.catch(policy: .allErrors) { error in
             error.isCancelled ? ex.fulfill() : XCTFail("Error: \(error)")
-        }
-        context.cancel()
+        }.cancel()
         
         waitForExpectations(timeout: 1)
         
