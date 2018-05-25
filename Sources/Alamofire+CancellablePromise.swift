441c536d85415c57ac13738f18555c40efa28773
dougzilla <dougzilla32@gmail.com>
2018-05-24 23:47:45 -0700

    Create CancellablePromise, CancellableGuarantee, CancellableCatchMixin, and CancellableThenable as wrappers that support cancellation
    Change the access control for 'CC' functions and methods from 'public' to 'internal', use the CancellablePromise API instead
    
    Clarify error messages
    
    Add RaceTests and RegressionTests

--- Sources/Alamofire+CancellablePromise.swift
+++ Sources/Alamofire+CancellablePromise.swift
@@ -1,7 +1,7 @@
 import Alamofire
 import PromiseKit
 #if !CPKCocoaPods
-import CancelForPromiseKit
+@testable import CancelForPromiseKit
 #endif
 
 extension Request: CancellableTask {
@@ -11,49 +11,63 @@ extension Request: CancellableTask {
 }
 
 extension Alamofire.DataRequest {
-    func wrap<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
-        return Promise(cancel: cancel ?? CancelContext(), task: self) {seal in
+    func wrap<T>(_ promise: Promise<T>) -> CancellablePromise<T> {
+        return CancellablePromise(task: self) { seal in
             promise.done {
                 seal.fulfill($0)
-                }.catch {
-                    seal.reject($0)
+            }.catch {
+                seal.reject($0)
+            }
+        }
+    }
+    
+   func wrapCC<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
+        return Promise(cancel: cancel ?? CancelContext(), task: self) { seal in
+            promise.done {
+                seal.fulfill($0)
+            }.catch {
+                seal.reject($0)
             }
         }
     }
 
-    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(URLRequest, HTTPURLResponse, Data)> {
-        return wrap(self.response(.promise, queue: queue), cancel: cancel)
+    public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil) -> CancellablePromise<(URLRequest, HTTPURLResponse, Data)> {
+        return wrap(self.response(.promise, queue: queue))
+    }
+    
+    public func responseCN(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(URLRequest, HTTPURLResponse, Data)> {
+        return wrapCC(self.response(.promise, queue: queue), cancel: cancel)
     }
     
     public func responseDataCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(data: Data, response: PMKAlamofireDataResponse)> {
-        return wrap(self.responseData(queue: queue), cancel: cancel)
+        return wrapCC(self.responseData(queue: queue), cancel: cancel)
     }
     
     public func responseStringCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<(string: String, response: PMKAlamofireDataResponse)> {
-        return wrap(self.responseString(queue: queue), cancel: cancel)
+        return wrapCC(self.responseString(queue: queue), cancel: cancel)
     }
     
     public func responseJSONCC(queue: DispatchQueue? = nil, options: JSONSerialization.ReadingOptions = .allowFragments, cancel: CancelContext? = nil) -> Promise<(json: Any, response: PMKAlamofireDataResponse)> {
-        return wrap(self.responseJSON(queue: queue, options: options), cancel: cancel)
+        return wrapCC(self.responseJSON(queue: queue, options: options), cancel: cancel)
     }
     
     public func responsePropertyListCC(queue: DispatchQueue? = nil, options: PropertyListSerialization.ReadOptions = PropertyListSerialization.ReadOptions(), cancel: CancelContext? = nil) -> Promise<(plist: Any, response: PMKAlamofireDataResponse)> {
-        return wrap(self.responsePropertyList(queue: queue, options: options), cancel: cancel)
+        return wrapCC(self.responsePropertyList(queue: queue, options: options), cancel: cancel)
     }
     
     #if swift(>=3.2)
     public func responseDecodableCC<T: Decodable>(queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder(), cancel: CancelContext? = nil) -> Promise<T> {
-        return wrap(self.responseDecodable(queue: queue, decoder: decoder), cancel: cancel)
+        return wrapCC(self.responseDecodable(queue: queue, decoder: decoder), cancel: cancel)
     }
     
     public func responseDecodableCC<T: Decodable>(_ type: T.Type, queue: DispatchQueue? = nil, decoder: JSONDecoder = JSONDecoder(), cancel: CancelContext? = nil) -> Promise<T> {
-        return wrap(self.responseDecodable(type, queue: queue, decoder: decoder), cancel: cancel)
+        return wrapCC(self.responseDecodable(type, queue: queue, decoder: decoder), cancel: cancel)
     }
     #endif
 }
 
 extension Alamofire.DownloadRequest {
-    func wrap<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
+    func wrapCC<T>(_ promise: Promise<T>, cancel: CancelContext?) -> Promise<T> {
         return Promise(cancel: cancel ?? CancelContext(), task: self) {seal in
             promise.done {
                 seal.fulfill($0)
@@ -64,10 +78,10 @@ extension Alamofire.DownloadRequest {
     }
 
     public func responseCC(_: PMKNamespacer, queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<DefaultDownloadResponse> {
-        return wrap(self.response(.promise, queue: queue), cancel: cancel)
+        return wrapCC(self.response(.promise, queue: queue), cancel: cancel)
     }
     
     public func responseDataCC(queue: DispatchQueue? = nil, cancel: CancelContext? = nil) -> Promise<DownloadResponse<Data>> {
-        return wrap(self.responseData(queue: queue), cancel: cancel)
+        return wrapCC(self.responseData(queue: queue), cancel: cancel)
     }
 }
