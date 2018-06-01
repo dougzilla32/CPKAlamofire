12486f1c01077db98b249d9b0aef0709781c6bd8
dougzilla <dougzilla32@gmail.com>
2018-05-31 22:43:49 -0700

    Update the Swift Package Manager files to use 'CancelForPromiseKit' (rather than the old 'CancellablePromiseKit')

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "CPKAlamofire",
     dependencies: [
-        .Package(url: "https://github.com/dougzilla32/CancellablePromiseKit", majorVersion: 1),
+        .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit", majorVersion: 1),
         .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
     ],
     exclude: ["Tests"]
