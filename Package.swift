397157b96d3ed9de18a59787fba869dfce55c41f
dougzilla <dougzilla32@gmail.com>
2018-08-05 23:10:43 -0700

    Update SPM config files to include appropriate minor version number for dependencies
    Update SPM config files to use '.git' suffix when specifying dependencies, to match the PromiseKit convention

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "CPKAlamofire",
     dependencies: [
         .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
-        .Package(url: "https://github.com/PromiseKit/Alamofire-", majorVersion: 3),
-        .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit", majorVersion: 1)
+        .Package(url: "https://github.com/PromiseKit/Alamofire-.git", majorVersion: 3, minor: 2),
+        .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit.git", majorVersion: 1, minor: 1)
     ],
     exclude: ["Tests"]
 )
