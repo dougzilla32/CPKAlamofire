8f62a8318f6ae30d0ebca5bc97a6852232b2f29b
Doug <dougstein@gmail.com>
2018-08-06 18:13:37 -0700

    Update SPM config to 4.0, but does not work because the PMKAlamofire SPM config file version 3.0 is unworkable

--- Package.swift
+++ Package.swift
@@ -1,11 +1,30 @@
-import PackageDescription
+// swift-tools-version:4.0
 
-let package = Package(
-    name: "CPKAlamofire",
-    dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
-        .Package(url: "https://github.com/PromiseKit/Alamofire-.git", majorVersion: 3, minor: 2),
-        .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit.git", majorVersion: 1, minor: 1)
-    ],
-    exclude: ["Tests"]
-)
+// Cannot current support SPM because PMKAlamofire's outdated SPM config file causes this error:
+//   error: dependency graph is unresolvable; found these conflicting requirements:
+//
+//   Dependencies:
+//     https://github.com/PromiseKit/Alamofire.git @ 3.2.0..<4.0.0
+
+//import PackageDescription
+//
+//let pkg = Package(name: "CPKAlamofire")
+//pkg.products = [
+//    .library(name: "CPKAlamofire", targets: ["CPKAlamofire"]),
+//]
+//pkg.dependencies = [
+//    .package(url: "https://github.com/dougzilla32/CancelForPromiseKit.git", from: "1.1.0"),
+//    .package(url: "https://github.com/PromiseKit/Alamofire.git", from: "3.2.0")
+////    .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "6.0.0")
+//]
+//
+//let cpkaf: Target = .target(name: "CPKAlamofire")
+//cpkaf.path = "Sources"
+//cpkaf.dependencies = ["CancelForPromiseKit", "PMKAlamofire"]
+//
+//pkg.swiftLanguageVersions = [3, 4]
+//pkg.targets = [
+//    cpkaf
+//// Cannot run tests because OHHTTPStubs does not currently support SPM
+////    .testTarget(name: "CPKAFTests", dependencies: ["CPKFoundation", "OHHTTPStubs"], path: "Tests"),
+//]
